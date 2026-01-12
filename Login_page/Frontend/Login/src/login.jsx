92ae4eb5f82165c89fdba7dccbb00c4a062c3553
Manan <mananbhayani4@gmail.com>
2026-01-12 12:03:58 +0530

    added class component into login.jsx

--- Login_page/Frontend/Login/src/login.jsx
+++ Login_page/Frontend/Login/src/login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+/*import React, { useState } from "react";
 import {
   Dialog,
   DialogTitle,
@@ -70,7 +70,6 @@ function Login({ onSuccess, goSignup }) {
         </form>
       </div>
 
-      {/* 🔔 ERROR DIALOG */}
       <Dialog open={dialogOpen} onClose={() => setDialogOpen(false)}>
         <DialogTitle>Login Error</DialogTitle>
         <DialogContent>{dialogMessage}</DialogContent>
@@ -80,6 +79,106 @@ function Login({ onSuccess, goSignup }) {
       </Dialog>
     </div>
   );
+}
+
+export default Login;*/
+
+
+import React, { useState } from "react";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+} from "@mui/material";
+import "./login.css";
+
+class Login extends React.Component({ onSuccess, goSignup }) {
+  constructor(props){
+    super(props);
+    this.state={
+      email: "",
+      password: "",
+      dialogOpen: false,
+      dialogMessage: ""
+    };
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    const { email, password } = this.state;
+
+    try{
+    const res = await fetch("http://localhost:5000/login-verify", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+
+    const data = await res.json();
+
+    if (!res.ok) {
+      this.setState({dialogOpen: true, dialogMessage: data.message});
+    } else {
+      this.props.onSuccess();
+    }
+  }
+    catch(error){
+      this.setState({dialogOpen: true, dialogMessage: "Login failed! Please try again."});
+    }
+  };
+  render (){
+    const { email, password, dialogOpen, dialogMessage } = this.state;
+    const{ onSuccess, goSignup } = this.props;
+
+    return (
+    <div className="mainbg">
+      <div className="middle">
+        <h2 className="welcome">Login</h2>
+
+        <form onSubmit={handleSubmit}>
+          <input
+            placeholder="Email"
+            className="form-control emaildiv"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            autoFocus
+          />
+
+          <input
+            type="password"
+            placeholder="Password"
+            className="form-control passworddiv"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button className="glow-on-hover button">Login</button>
+
+          <p className="switch newhere">
+            New here?
+            <button
+              type="button"
+              className="link-btn regibtn"
+              onClick={goSignup}
+            >
+              Register
+            </button>
+          </p>
+        </form>
+      </div>
+
+      <Dialog open={dialogOpen} onClose={() => this.setState({dialogOpen: false})}>
+        <DialogTitle>Login Error</DialogTitle>
+        <DialogContent>{dialogMessage}</DialogContent>
+        <DialogActions>
+          <Button onClick={() => this.setState({dialogOpen: false})}>OK</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
 }
 
 export default Login;
