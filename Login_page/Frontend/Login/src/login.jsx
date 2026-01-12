4af3192599bceef495022b84c3d7ec88be1f124b
Manan <mananbhayani4@gmail.com>
2026-01-12 15:54:55 +0530

    removed class in login jsx

--- Login_page/Frontend/Login/src/login.jsx
+++ Login_page/Frontend/Login/src/login.jsx
@@ -1,4 +1,4 @@
-/*import React, { useState } from "react";
+import React, { useState } from "react";
 import {
   Dialog,
   DialogTitle,
@@ -16,20 +16,24 @@ function Login({ onSuccess, goSignup }) {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    try {
+      const res = await fetch("http://localhost:5000/login-verify", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      });
 
-    const res = await fetch("http://localhost:5000/login-verify", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, password }),
-    });
+      const data = await res.json();
 
-    const data = await res.json();
-
-    if (!res.ok) {
-      setDialogMessage(data.message);
+      if (!res.ok) {
+        setDialogMessage(data.message);
+        setDialogOpen(true);
+      } else {
+        onSuccess();
+      }
+    } catch (error) {
+      setDialogMessage("Login failed! Please try again.");
       setDialogOpen(true);
-    } else {
-      onSuccess();
     }
   };
 
@@ -79,112 +83,6 @@ function Login({ onSuccess, goSignup }) {
       </Dialog>
     </div>
   );
-}
-
-export default Login;*/
-
-import React, { useState } from "react";
-import {
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogActions,
-  Button,
-} from "@mui/material";
-import "./login.css";
-
-class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: "",
-      password: "",
-      dialogOpen: false,
-      dialogMessage: "",
-    };
-  }
-
-  handleSubmit = async (e) => {
-    e.preventDefault();
-    const { email, password } = this.state;
-
-    try {
-      const res = await fetch("http://localhost:5000/login-verify", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password }),
-      });
-
-      const data = await res.json();
-
-      if (!res.ok) {
-        this.setState({ dialogOpen: true, dialogMessage: data.message });
-      } else {
-        this.props.onSuccess();
-      }
-    } catch (error) {
-      this.setState({
-        dialogOpen: true,
-        dialogMessage: "Login failed! Please try again.",
-      });
-    }
-  };
-  render() {
-    const { email, password, dialogOpen, dialogMessage } = this.state;
-    const { onSuccess, goSignup } = this.props;
-
-    return (
-      <div className="mainbg">
-        <div className="middle">
-          <h2 className="welcome">Login</h2>
-
-          <form onSubmit={this.handleSubmit}>
-            <input
-              placeholder="Email"
-              className="form-control emaildiv"
-              value={email}
-              onChange={(e) => this.setState({ email: e.target.value })}
-              autoFocus
-            />
-
-            <input
-              type="password"
-              placeholder="Password"
-              className="form-control passworddiv"
-              value={password}
-              onChange={(e) => this.setState({ password: e.target.value })}
-            />
-
-            <button className="glow-on-hover button">Login</button>
-
-            <p className="switch newhere">
-              New here?
-              <button
-                type="button"
-                className="link-btn regibtn"
-                onClick={goSignup}
-              >
-                Register
-              </button>
-            </p>
-          </form>
-        </div>
-
-        <Dialog
-          open={dialogOpen}
-          onClose={() => this.setState({ dialogOpen: false })}
-        >
-          <DialogTitle>Login Error</DialogTitle>
-          <DialogContent>{dialogMessage}</DialogContent>
-          <DialogActions>
-            <Button onClick={() => this.setState({ dialogOpen: false })}>
-              OK
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </div>
-    );
-  }
 }
 
 export default Login;
